d3c357a595bf75977f1761a615edd14da1cb794b
perolajanice <perolajanice1@gmail.com>
2018-01-10 17:04:36 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var words = [
+  "Self-care isn't selfish", 
+  "Be happy", 
+  "Self love is the best love",
+  "a quote",
+  "another quote"
+  ];
+
+
+function setup() {
+  createCanvas(1000, 600);
+  background(255, 190, 230);
+  
+  createButton
+}
+
+
+function draw() {
+ if (keyIsPressed === true) {
+ var word = random(words);
+ text(word, random(width),random(height))
+ } 
+}
+
+
--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+var randomWordGenerator = function (number) {
+	  var word = '';
+
+	  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+	  for (var i = 0; i < number; i++) {
+	    word += alphabet[Math.floor(Math.random() * alphabet.length)];
+	  }
+
+	  return word;
+	};
+
+	console.log(randomWordGenerator(5));
