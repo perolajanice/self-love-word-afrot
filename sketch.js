00d64abc02b2a1369f2c4fa16c39531b5d30c5f2
perolajanice <perolajanice1@gmail.com>
2018-01-18 21:32:47 +0000

    new version - changes 2

--- sketch.js
+++ sketch.js
@@ -1,25 +1,39 @@
+var button;
+
 var words = [
-  "Self-care isn't selfish", 
-  "Be happy", 
+  "Self-care isn't selfish",
+  "Your mind must arrive at the destination before your life does",
   "Self love is the best love",
-  "a quote",
-  "another quote"
+  "a quotea quotea quotea quotea quote",
+  "another quote another quoteanother quoteanother quote"
   ];
 
-
 function setup() {
-  createCanvas(1000, 600);
+  createCanvas(1800, 990);
   background(255, 190, 230);
-  
-  createButton
+  button = createButton('click me');
+  button.position(800, 450);
+  button.mousePressed(changeWord);
 }
-
 
 function draw() {
- if (keyIsPressed === true) {
- var word = random(words);
- text(word, random(width),random(height))
- } 
+
+  var title = 'Wall of Self-Love';
+  text(title, 650, 200, textSize(40));
+
+  var instructions = 'Click button to generate quotes and press a key to clear wall :)';
+  text(instructions, 100, 970, textSize(17));
+
+
+  if (keyIsPressed === true) {
+    background(255, 190, 230);
+  }
 }
 
 
+function changeWord() {
+ var word = random(words);
+
+ textSize(15);
+ text(word, random(1600),random(900), 150, 150);
+}
